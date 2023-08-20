fd74b4de989516f4219c56f65b5bc2d0bb7752e2
Musa Waseem <66893046+MusaW650@users.noreply.github.com>
2023-08-19 23:52:06 -0400

    Add files via upload

---
+++ Pensando Gobierno/Pensando Gobierno/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Pensando Gobierno
+//
+//  Created by Musa W on 10/9/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ Pensando Gobierno/Pensando Gobierno/Pensando_GobiernoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pensando_GobiernoApp.swift
+//  Pensando Gobierno
+//
+//  Created by Musa W on 10/9/20.
+//
+
+import SwiftUI
+
+@main
+struct Pensando_GobiernoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
